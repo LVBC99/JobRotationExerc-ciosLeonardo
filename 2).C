c4840534a497a0b53a9791fab6809bfc6c8e0f22
LVBC99 <95540658+LVBC99@users.noreply.github.com>
2023-03-23 20:20:30 -0300

    Update 2).C

--- 2).C
+++ 2).C
@@ -1,1 +1,20 @@
-a
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num, m, fib=0, aux=1;
+    printf("Informe um numero para calcular a sequencia de Fibonacci:\n ");
+    scanf("%d", &num);
+    m = num;
+    while(fib <1000){
+        printf("%d-", fib);
+        fib = fib + aux;
+        aux = num;
+        num = fib;
+    }
+    if (m==0||m==1){
+        printf("\nO numero informado pertence a sequencia");
+    }
+  return 0;
+}
+
